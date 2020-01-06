550e95a092babb8ee7cbfa1b580e9bf0ae5b1c5b
Amanpreet <amanpreet.singh@mymindinfotech.com>
2020-01-06 20:46:29 +0530

    Add project files.

---
+++ MySqlConnectivity/EmployeeContext.cs
@@ -0,0 +1,16 @@
+﻿using MySql.Data.Entity;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySqlConnectivity
+{
+    [DbConfigurationType(typeof(MySqlEFConfiguration))]
+    public class EmployeeContext : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
